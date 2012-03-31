92f1eed8626fe1006628fd4e3ac1e5294ce95e80
nobodyAtall <tsogkas@ceid.upatras.gr>
2012-04-01 00:07:49 +0300

    Ultra brightness support and ability to toggle via notification bar buttons (2/2)
    Change-Id: I9e30daa97570294f120c968d5ede0a289a552f4a

--- src/com/android/settings/BrightnessPreference.java
+++ src/com/android/settings/BrightnessPreference.java
@@ -34,15 +34,21 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.SeekBar;
+import java.io.FileWriter;
+import java.io.IOException;
+import android.util.Log;
 
 public class BrightnessPreference extends SeekBarDialogPreference implements
         SeekBar.OnSeekBarChangeListener, CheckBox.OnCheckedChangeListener {
 
     private SeekBar mSeekBar;
     private CheckBox mCheckBox;
+    private CheckBox mCheckBoxUltra;
 
     private int mOldBrightness;
     private int mOldAutomatic;
+    private int mOldUltraBrightness;
+    private static final String TAG = "BrightnessPreference";
 
     private boolean mAutomaticAvailable;
 
@@ -68,6 +74,13 @@ public class BrightnessPreference extends SeekBarDialogPreference implements
         }
     };
 
+    private ContentObserver mBrightnessModeUltraObserver = new ContentObserver(new Handler()) {
+        @Override
+        public void onChange(boolean selfChange) {
+            onBrightnessUltraModeChanged();
+        }
+    };
+
     public BrightnessPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
 
@@ -89,6 +102,11 @@ public class BrightnessPreference extends SeekBarDialogPreference implements
         getContext().getContentResolver().registerContentObserver(
                 Settings.System.getUriFor(Settings.System.SCREEN_BRIGHTNESS_MODE), true,
                 mBrightnessModeObserver);
+
+        getContext().getContentResolver().registerContentObserver(
+                Settings.System.getUriFor(Settings.System.SCREEN_BRIGHTNESS_ULTRA_MODE), true,
+                mBrightnessModeUltraObserver);
+
         mRestoredOldState = false;
     }
 
@@ -110,6 +128,11 @@ public class BrightnessPreference extends SeekBarDialogPreference implements
             mCheckBox.setVisibility(View.GONE);
         }
         mSeekBar.setOnSeekBarChangeListener(this);
+        mOldUltraBrightness = getUltraBrightnessMode(0);
+        Log.d(TAG,"mOldUltraBrighess was found as " + mOldUltraBrightness);
+        mCheckBoxUltra = (CheckBox)view.findViewById(R.id.ultra_mode);
+        mCheckBoxUltra.setOnCheckedChangeListener(this);
+        mCheckBoxUltra.setChecked(mOldUltraBrightness != 0);
     }
 
     public void onProgressChanged(SeekBar seekBar, int progress,
@@ -126,6 +149,16 @@ public class BrightnessPreference extends SeekBarDialogPreference implements
     }
 
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if (buttonView == mCheckBoxUltra) {
+            Log.d(TAG,"ultrabrightess set to "+isChecked);
+            if (isChecked)
+                writeOneLine("/sys/devices/platform/i2c-adapter/i2c-0/0-0036/mode", "i2c_pwm");
+            else
+                writeOneLine("/sys/devices/platform/i2c-adapter/i2c-0/0-0036/mode", "i2c_pwm_als");
+            setUltraMode(isChecked ? Settings.System.SCREEN_BRIGHTNESS_MODE_ULTRA_ENABLED
+                : Settings.System.SCREEN_BRIGHTNESS_MODE_ULTRA_DISABLED);
+            return;
+        }
         setMode(isChecked ? Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC
                 : Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
         if (!isChecked) {
@@ -153,6 +186,16 @@ public class BrightnessPreference extends SeekBarDialogPreference implements
         return brightnessMode;
     }
 
+    private int getUltraBrightnessMode(int defaultValue) {
+        int ultraMode = defaultValue;
+        try {
+            ultraMode = Settings.System.getInt(getContext().getContentResolver(),
+                    Settings.System.SCREEN_BRIGHTNESS_ULTRA_MODE);
+        } catch (SettingNotFoundException snfe) {
+        }
+        return ultraMode;
+    }
+
     private void onBrightnessChanged() {
         int brightness = getBrightness(MAXIMUM_BACKLIGHT);
         mSeekBar.setProgress(brightness - mScreenBrightnessDim);
@@ -161,6 +204,12 @@ public class BrightnessPreference extends SeekBarDialogPreference implements
     private void onBrightnessModeChanged() {
         boolean checked = getBrightnessMode(0) != 0;
         mCheckBox.setChecked(checked);
+    }
+
+    private void onBrightnessUltraModeChanged() {
+        boolean checked = getUltraBrightnessMode(0) != 0;
+        mCheckBoxUltra.setChecked(checked);
+        Log.d(TAG,"onBrightnessUltraModeChanged! " + checked);
     }
 
     @Override
@@ -179,6 +228,7 @@ public class BrightnessPreference extends SeekBarDialogPreference implements
 
         resolver.unregisterContentObserver(mBrightnessObserver);
         resolver.unregisterContentObserver(mBrightnessModeObserver);
+        resolver.unregisterContentObserver(mBrightnessModeUltraObserver);
     }
 
     private void restoreOldState() {
@@ -190,6 +240,7 @@ public class BrightnessPreference extends SeekBarDialogPreference implements
         if (!mAutomaticAvailable || mOldAutomatic == 0) {
             setBrightness(mOldBrightness);
         }
+        setUltraMode(mOldUltraBrightness);
         mRestoredOldState = true;
     }
 
@@ -215,6 +266,12 @@ public class BrightnessPreference extends SeekBarDialogPreference implements
                 Settings.System.SCREEN_BRIGHTNESS_MODE, mode);
     }
 
+    private void setUltraMode(int mode) {
+        Log.d(TAG,"Setting ultramode to " + mode);
+        Settings.System.putInt(getContext().getContentResolver(),
+                Settings.System.SCREEN_BRIGHTNESS_ULTRA_MODE, mode);
+    }
+
     @Override
     protected Parcelable onSaveInstanceState() {
         final Parcelable superState = super.onSaveInstanceState();
@@ -223,8 +280,11 @@ public class BrightnessPreference extends SeekBarDialogPreference implements
         // Save the dialog state
         final SavedState myState = new SavedState(superState);
         myState.automatic = mCheckBox.isChecked();
+        myState.ultra = mCheckBoxUltra.isChecked();
+        Log.d(TAG,"Saved ultra: " + myState.ultra);
         myState.progress = mSeekBar.getProgress();
         myState.oldAutomatic = mOldAutomatic == 1;
+        myState.oldUltra = mOldUltraBrightness == 1;
         myState.oldProgress = mOldBrightness;
 
         // Restore the old state when the activity or dialog is being paused
@@ -244,7 +304,10 @@ public class BrightnessPreference extends SeekBarDialogPreference implements
         super.onRestoreInstanceState(myState.getSuperState());
         mOldBrightness = myState.oldProgress;
         mOldAutomatic = myState.oldAutomatic ? 1 : 0;
+        mOldUltraBrightness = myState.oldUltra ? 1 : 0;
+        Log.d(TAG,"mOldUltraBrightness restored to " + mOldUltraBrightness);
         setMode(myState.automatic ? 1 : 0);
+        setUltraMode(myState.ultra ? 1 : 0);
         setBrightness(myState.progress + mScreenBrightnessDim);
     }
 
@@ -252,14 +315,18 @@ public class BrightnessPreference extends SeekBarDialogPreference implements
 
         boolean automatic;
         boolean oldAutomatic;
+        boolean ultra;
+        boolean oldUltra;
         int progress;
         int oldProgress;
 
         public SavedState(Parcel source) {
             super(source);
             automatic = source.readInt() == 1;
+            ultra = source.readInt() == 1;
             progress = source.readInt();
             oldAutomatic = source.readInt() == 1;
+            oldUltra = source.readInt() == 1;
             oldProgress = source.readInt();
         }
 
@@ -267,8 +334,10 @@ public class BrightnessPreference extends SeekBarDialogPreference implements
         public void writeToParcel(Parcel dest, int flags) {
             super.writeToParcel(dest, flags);
             dest.writeInt(automatic ? 1 : 0);
+            dest.writeInt(ultra ? 1 : 0);
             dest.writeInt(progress);
             dest.writeInt(oldAutomatic ? 1 : 0);
+            dest.writeInt(oldUltra ? 1 : 0);
             dest.writeInt(oldProgress);
         }
 
@@ -288,5 +357,22 @@ public class BrightnessPreference extends SeekBarDialogPreference implements
             }
         };
     }
+
+    public static boolean writeOneLine(String fname, String value) {
+        try {
+            FileWriter fw = new FileWriter(fname);
+            try {
+                fw.write(value);
+            } finally {
+                fw.close();
+            }
+        } catch (IOException e) {
+            String Error = "Error writing to " + fname + ". Exception: ";
+            Log.e(TAG, Error, e);
+            return false;
+        }
+        return true;
+    }
+
 }
 
