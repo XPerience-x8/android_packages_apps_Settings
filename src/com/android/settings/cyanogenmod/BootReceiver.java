3ad5c1ad798f5ef2381f98554c4e14ddfbef0797
nobodyAtall <tsogkas@ceid.upatras.gr>
2012-06-08 16:53:32 +0300

    Implementing Swap to SDcard support via Settings -> Performance -> Memory management
    Change-Id: I5d393dc818484485b4cbef593da284990b8c5443

--- src/com/android/settings/cyanogenmod/BootReceiver.java
+++ src/com/android/settings/cyanogenmod/BootReceiver.java
@@ -20,11 +20,15 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.os.SystemProperties;
 import android.preference.PreferenceManager;
 import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.android.settings.Utils;
 
@@ -43,10 +47,16 @@ public class BootReceiver extends BroadcastReceiver {
     private static final String CPU_SETTINGS_PROP = "sys.cpufreq.restored";
     private static final String KSM_SETTINGS_PROP = "sys.ksm.restored";
     private static final String UNDERVOLTING_PROP = "persist.sys.undervolt";
+    private static final String SWAP_FILE = "/mnt/sdcard/.MiniCM9.swp";
+    private static final String SWAP_FILE_STAGING = "/mnt/secure/staging/.MiniCM9.swp";
+    private static final String SWAP_ENABLED_PROP = "persist.sys.swap.enabled";
+    private static final String SWAP_SIZE_PROP = "persist.sys.swap.size";
     private static String UV_MODULE;
+    private static Context myContext;
 
     @Override
     public void onReceive(Context ctx, Intent intent) {
+        myContext = ctx;
         if (SystemProperties.getBoolean(CPU_SETTINGS_PROP, false) == false
                 && intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
             SystemProperties.set(CPU_SETTINGS_PROP, "true");
@@ -65,12 +75,83 @@ public class BootReceiver extends BroadcastReceiver {
             }
         }
 
-        if (getUltraBrightnessMode(ctx, 0) == 1)
-            writeOneLine("/sys/devices/platform/i2c-adapter/i2c-0/0-0036/mode", "i2c_pwm");
-        else
-            writeOneLine("/sys/devices/platform/i2c-adapter/i2c-0/0-0036/mode", "i2c_pwm_als");
+        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+            if (getUltraBrightnessMode(ctx, 0) == 1)
+                writeOneLine("/sys/devices/platform/i2c-adapter/i2c-0/0-0036/mode", "i2c_pwm");
+            else
+                writeOneLine("/sys/devices/platform/i2c-adapter/i2c-0/0-0036/mode", "i2c_pwm_als");
+        }
+
+        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED) || intent.getAction().equals(Intent.ACTION_MEDIA_MOUNTED)) {
+            if (SystemProperties.get(SWAP_ENABLED_PROP).equals("1")) {
+                Log.d(TAG, "SWAP_ENABLED_PROP: " + SWAP_ENABLED_PROP);
+                String command = "swapon " + SWAP_FILE;
+                mrunShellCommand = new runShellCommand(command, ctx.getResources().getString(com.android.settings.R.string.swap_toast_swap_enabled));
+                mrunShellCommand.start();
+            }
+        }
+
+        else if ( intent.getAction().equals(Intent.ACTION_SHUTDOWN) || intent.getAction().equals(Intent.ACTION_MEDIA_EJECT)) {
+            if (SystemProperties.get(SWAP_ENABLED_PROP).equals("1")) {
+                Log.d(TAG, "SWAP_ENABLED_PROP: " + SWAP_ENABLED_PROP);
+                String command = "swapoff " + SWAP_FILE_STAGING;
+                mrunShellCommand = new runShellCommand(command, ctx.getResources().getString(com.android.settings.R.string.swap_toast_swap_disabled));
+                mrunShellCommand.start();
+            }
+        }
 
     }
+
+    private class runShellCommand extends Thread {
+        private String command = "";
+        private String toastMessage = "";
+
+        public runShellCommand(String command, String toastMessage) {
+            this.command = command;
+            this.toastMessage = toastMessage;
+        }
+
+        @Override
+        public void run() {
+        try {
+            if (!command.equals("")) {
+                Process process = Runtime.getRuntime().exec("su");
+                Log.d(TAG, "Executing: " + command);
+                DataOutputStream outputStream = new DataOutputStream(process.getOutputStream());
+                DataInputStream inputStream = new DataInputStream(process.getInputStream());
+                outputStream.writeBytes(command + "\n");
+                outputStream.flush();
+                outputStream.writeBytes("exit\n");
+                outputStream.flush();
+                process.waitFor();
+            }
+            } catch (IOException e) {
+                Log.e(TAG, "Thread IOException");
+            }
+            catch (InterruptedException e) {
+                Log.e(TAG, "Thread InterruptedException");
+            }
+            Message messageToThread = new Message();
+            Bundle messageData = new Bundle();
+            messageToThread.what = 0;
+            messageData.putString("toastMessage", toastMessage);
+            messageToThread.setData(messageData);
+            mrunShellCommandHandler.sendMessage(messageToThread);
+        }
+    };
+
+    private runShellCommand mrunShellCommand;
+
+    private Handler mrunShellCommandHandler = new Handler() {
+        public void handleMessage(Message msg) {
+            CharSequence text="";
+            Bundle messageData = msg.getData();
+            text = messageData.getString("toastMessage", "");
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(myContext, text, duration);
+            toast.show();
+        }
+    };
 
     private void configureCPU(Context ctx) {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(ctx);
@@ -89,7 +170,7 @@ public class BootReceiver extends BroadcastReceiver {
         else {
             // remove undervolting module
             //insmod(UV_MODULE, false);
-	}
+	    }
 
         String governor = prefs.getString(Processor.GOV_PREF, null);
         String minFrequency = prefs.getString(Processor.FREQ_MIN_PREF, null);
@@ -165,7 +246,7 @@ public class BootReceiver extends BroadcastReceiver {
         command = "/system/bin/rmmod " + module;
         try {
             Process process = Runtime.getRuntime().exec("su");
-            Log.e(TAG, "Executing: " + command);
+            Log.d(TAG, "Executing: " + command);
             DataOutputStream outputStream = new DataOutputStream(process.getOutputStream()); 
             DataInputStream inputStream = new DataInputStream(process.getInputStream());
             outputStream.writeBytes(command + "\n");
